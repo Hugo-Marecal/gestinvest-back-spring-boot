225ed2191480c9d2d72762a1f42abb7af3f7cb86
h-dev <hugo.marecal@oclock.school>
2024-11-13 10:49:06 +0100

    setting up getAccountInfos, UserDto

--- src/main/java/dev/gest/invest/controller/AccountController.java
+++ src/main/java/dev/gest/invest/controller/AccountController.java
@@ -1,6 +1,8 @@
 package dev.gest.invest.controller;
 
+import dev.gest.invest.dto.UserDto;
 import dev.gest.invest.model.User;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
 
     @GetMapping("/")
-    public User getAccountInfos(@AuthenticationPrincipal User user) {
-        return user;
+    public ResponseEntity<UserDto> getAccountInfos(@AuthenticationPrincipal User user) {
+        UserDto userDto = new UserDto(user.getEmail());
+
+        return ResponseEntity.ok(userDto);
     }
 }
---
+++ src/main/java/dev/gest/invest/dto/UserDto.java
@@ -0,0 +1,21 @@
+package dev.gest.invest.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDto {
+
+    @NotBlank(message = "Email is required")
+    @Email
+    private String email;
+    private String last_name;
+    private String first_name;
+
+    public UserDto(String email) {
+        this.email = email;
+    }
+}
