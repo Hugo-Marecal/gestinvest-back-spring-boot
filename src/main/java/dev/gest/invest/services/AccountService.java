ec20c933161ff046daa3f049b6ad2bf4240ec81b
h-dev <hugo.marecal@oclock.school>
2024-11-15 12:07:55 +0100

    delete some useless verification for updating lastName and firstName

--- src/main/java/dev/gest/invest/services/AccountService.java
+++ src/main/java/dev/gest/invest/services/AccountService.java
@@ -4,25 +4,18 @@ import dev.gest.invest.dto.UpdateUserDto;
 import dev.gest.invest.dto.UserDto;
 import dev.gest.invest.model.User;
 import dev.gest.invest.repository.UserRepository;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
 
-    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
-    public AccountService(PasswordEncoder passwordEncoder, UserRepository userRepository) {
-        this.passwordEncoder = passwordEncoder;
+    public AccountService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     public UserDto updateAccountInfos(User user, UpdateUserDto updateUserDto) {
-        if (!passwordEncoder.matches(updateUserDto.getPassword(), user.getPassword())) {
-            throw new BadCredentialsException("Auth failed");
-        }
 
         User userData = userRepository.findByEmail(user.getEmail())
                 .orElseThrow(() -> new IllegalArgumentException("User not found"));
@@ -33,10 +26,6 @@ public class AccountService {
 
         if (userData.getLast_name() == null || !userData.getLast_name().equalsIgnoreCase(updateUserDto.getLast_name())) {
             userData.setLast_name(updateUserDto.getLast_name());
-        }
-
-        if (userData.getEmail() == null || !userData.getEmail().equalsIgnoreCase(updateUserDto.getEmail())) {
-            userData.setEmail(updateUserDto.getEmail());
         }
 
         User updateUser = userRepository.save(userData);
