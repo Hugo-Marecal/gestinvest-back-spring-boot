00c325558b66b31ee4e36469113933753e36f3e3
h-dev <hugo.marecal@oclock.school>
2024-11-18 17:16:26 +0100

    Translate success or error messages in french

--- src/main/java/dev/gest/invest/config/ApplicationConfig.java
+++ src/main/java/dev/gest/invest/config/ApplicationConfig.java
@@ -23,7 +23,7 @@ public class ApplicationConfig {
     @Bean
     UserDetailsService userDetailsService() {
         return username -> userRepository.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+                .orElseThrow(() -> new UsernameNotFoundException("Utilisateur non trouvé"));
     }
 
     @Bean
